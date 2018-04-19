3c969d99fd1b487297c6cce877297db6819ff459
13lackninja <jonathan.crawford55@gmail.com>
2018-04-19 15:48:56 -0700

    updating names

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('<p style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-family: arial; font-size: 18px">Site under construction!</p>');
-});
-
-app.listen(8080);
